fe3a01b369d9b5aacd65f15bdba234d609c102e6
corralejo-htls <corralejo.hotels@gmail.com>
2026-03-07 04:17:39 +0000

    ok

--- start_celery.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM BookingScraper Pro v6.0 - Start Celery Worker
-REM Windows: --pool=threads is REQUIRED (no fork/gevent support)
-title BookingScraper Pro - Celery Worker
-cd /d "%~dp0"
-call .venv\Scripts\activate.bat
-echo Starting Celery worker (pool=threads)...
-celery -A app.celery_app worker --pool=threads --concurrency=4 --loglevel=info --logfile=logs\celery.log
-pause
